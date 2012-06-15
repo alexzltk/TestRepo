aaa5036a9189733b63eb46e9715aa20a3bc5a57c
Alex Zlatkin <alex.zltk@gmail.com>
2012-06-15 21:33:44 +0300

    change 1

--- TestApp/ClassView.cpp
+++ TestApp/ClassView.cpp
@@ -136,12 +136,12 @@ void CClassView::FillClassView()
 	HTREEITEM hRoot = m_wndClassView.InsertItem(_T("FakeApp classes"), 0, 0);
 	m_wndClassView.SetItemState(hRoot, TVIS_BOLD, TVIS_BOLD);
 
-	HTREEITEM hClass = m_wndClassView.InsertItem(_T("CFakeAboutDlg"), 1, 1, hRoot);
+	HTREEITEM hClass = m_wndClassView.InsertItem(_T("CFakeAboutDlg1"), 1, 1, hRoot);
 	m_wndClassView.InsertItem(_T("CFakeAboutDlg()"), 3, 3, hClass);
 
 	m_wndClassView.Expand(hRoot, TVE_EXPAND);
 
-	hClass = m_wndClassView.InsertItem(_T("CFakeApp"), 1, 1, hRoot);
+	hClass = m_wndClassView.InsertItem(_T("CFakeApp1"), 1, 1, hRoot);
 	m_wndClassView.InsertItem(_T("CFakeApp()"), 3, 3, hClass);
 	m_wndClassView.InsertItem(_T("InitInstance()"), 3, 3, hClass);
 	m_wndClassView.InsertItem(_T("OnAppAbout()"), 3, 3, hClass);
@@ -151,13 +151,13 @@ void CClassView::FillClassView()
 	m_wndClassView.InsertItem(_T("~CFakeAppDoc()"), 3, 3, hClass);
 	m_wndClassView.InsertItem(_T("OnNewDocument()"), 3, 3, hClass);
 
-	hClass = m_wndClassView.InsertItem(_T("CFakeAppView"), 1, 1, hRoot);
+	hClass = m_wndClassView.InsertItem(_T("CFakeAppView1"), 1, 1, hRoot);
 	m_wndClassView.InsertItem(_T("CFakeAppView()"), 4, 4, hClass);
 	m_wndClassView.InsertItem(_T("~CFakeAppView()"), 3, 3, hClass);
 	m_wndClassView.InsertItem(_T("GetDocument()"), 3, 3, hClass);
 	m_wndClassView.Expand(hClass, TVE_EXPAND);
 
-	hClass = m_wndClassView.InsertItem(_T("CFakeAppFrame"), 1, 1, hRoot);
+	hClass = m_wndClassView.InsertItem(_T("CFakeAppFrame1"), 1, 1, hRoot);
 	m_wndClassView.InsertItem(_T("CFakeAppFrame()"), 3, 3, hClass);
 	m_wndClassView.InsertItem(_T("~CFakeAppFrame()"), 3, 3, hClass);
 	m_wndClassView.InsertItem(_T("m_wndMenuBar"), 6, 6, hClass);
